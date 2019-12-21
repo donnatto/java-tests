5cb5a7206dfc1252229faf8a1f813c20691f3702
Edwin Dominguez <edwindominguez@hotmail.com>
2019-12-21 18:16:27 -0500

    implement one function for each test

--- src/main/java/com/donnatto/javatests/util/StringUtil.java
+++ src/main/java/com/donnatto/javatests/util/StringUtil.java
@@ -3,6 +3,11 @@ package com.donnatto.javatests.util;
 public class StringUtil {
 
     public static String repeat(String string, int times) {
+
+        if (times < 0) {
+            throw new IllegalArgumentException("Negative times not allowed");
+        }
+
         String result = "";
 
         for (int i = 0, n = times; i < n; i++) {
--- src/test/java/com/donnatto/javatests/util/StringUtilTest.java
+++ src/test/java/com/donnatto/javatests/util/StringUtilTest.java
@@ -8,12 +8,22 @@ import org.junit.Test;
 public class StringUtilTest {
 
     @Test
-    public void testRepeat() {
-
-        Assert.assertEquals("holaholahola", StringUtil.repeat("hola", 3) );
+    public void repeatOnce() {
         Assert.assertEquals("hola", StringUtil.repeat("hola", 1));
-
-
     }
 
+    @Test
+    public void repeatMultipleTimes() {
+        Assert.assertEquals("holaholahola", StringUtil.repeat("hola", 3));
+    }
+
+    @Test
+    public void repeatZeroTimes() {
+        Assert.assertEquals("", StringUtil.repeat("hola", 0));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void repeatNegativeTimes() {
+        StringUtil.repeat("hola", -1);
+    }
 }
