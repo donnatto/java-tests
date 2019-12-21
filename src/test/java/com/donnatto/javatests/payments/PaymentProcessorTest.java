8635b65586da5ffa3e729278cd6d8ab9501fa008
Edwin Dominguez <edwindominguez@hotmail.com>
2019-12-21 18:58:20 -0500

    refactor payment tests

--- src/test/java/com/donnatto/javatests/payments/PaymentProcessorTest.java
+++ src/test/java/com/donnatto/javatests/payments/PaymentProcessorTest.java
@@ -1,5 +1,6 @@
 package com.donnatto.javatests.payments;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -7,24 +8,35 @@ import static org.junit.Assert.*;
 
 public class PaymentProcessorTest {
 
+    private PaymentGateway paymentGateway;
+    private PaymentProcessor paymentProcessor;
+
+    // Before para que se ejecute setup antes que otros metodos
+    @Before
+    public void setup() {
+        paymentGateway = Mockito.mock(PaymentGateway.class);
+        paymentProcessor = new PaymentProcessor(paymentGateway);
+
+    }
+
     @Test
     public void paymentIsCorrect() {
-        PaymentGateway mock = Mockito.mock(PaymentGateway.class);
-        Mockito.when(mock.requestPayment((PaymentRequest) Mockito.any()))
+        // Preparacion de objetos
+        Mockito.when(paymentGateway.requestPayment((PaymentRequest) Mockito.any()))
                 .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.OK));
 
-        PaymentProcessor paymentProcessor = new PaymentProcessor(mock);
+        // Ejecucion del metodo
+        boolean result = paymentProcessor.makePayment(1000);
 
-        assertTrue(paymentProcessor.makePayment(1000));
+        // Comprobacion del resultado
+        assertTrue(result);
     }
 
     @Test
     public void paymentIsWrong() {
-        PaymentGateway mock = Mockito.mock(PaymentGateway.class);
-        Mockito.when(mock.requestPayment((PaymentRequest) Mockito.any()))
+        Mockito.when(paymentGateway.requestPayment((PaymentRequest) Mockito.any()))
                 .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.ERROR));
 
-        PaymentProcessor paymentProcessor = new PaymentProcessor(mock);
 
         assertFalse(paymentProcessor.makePayment(1000));
     }
