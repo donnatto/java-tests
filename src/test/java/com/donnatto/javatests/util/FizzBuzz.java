56b0cf4b6cf2029883e0fa9b2dc692e571c2d2c6
Edwin Dominguez <edwindominguez@hotmail.com>
2019-12-21 22:12:59 -0500

    add fizzbuzz challenge test implementation

--- src/test/java/com/donnatto/javatests/util/FizzBuzz.java
+++ src/test/java/com/donnatto/javatests/util/FizzBuzz.java
@@ -1,4 +1,10 @@
 package com.donnatto.javatests.util;
 
 public class FizzBuzz {
+
+    public static String fizzBuzz(Integer n) {
+
+        return n % 3 == 0 ? (n % 5 == 0 ? "FizzBuzz" : "Fizz") : (n % 5 == 0 ? "Buzz" : n.toString());
+
+    }
 }
--- src/test/java/com/donnatto/javatests/util/FizzBuzzShould.java
+++ src/test/java/com/donnatto/javatests/util/FizzBuzzShould.java
@@ -1,5 +1,35 @@
+package com.donnatto.javatests.util;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class FizzBuzzShould {
 
+    @Test
+    public void returnFizzIfDivisibleBy3() {
+        assertEquals("Fizz", FizzBuzz.fizzBuzz(3));
+        assertEquals("Fizz", FizzBuzz.fizzBuzz(6));
+        assertEquals("Fizz", FizzBuzz.fizzBuzz(9));
+    }
+
+    @Test
+    public void returnBuzzIfDivisibleBy5() {
+        assertEquals("Buzz", FizzBuzz.fizzBuzz(5));
+        assertEquals("Buzz", FizzBuzz.fizzBuzz(10));
+    }
+
+    @Test
+    public void returnFizzBuzzIfDivisibleBy3And5() {
+        assertEquals("FizzBuzz", FizzBuzz.fizzBuzz(15));
+        assertEquals("FizzBuzz", FizzBuzz.fizzBuzz(30));
+        assertEquals("FizzBuzz", FizzBuzz.fizzBuzz(45));
+    }
+
+    @Test
+    public void returnSameNumberIfOtherCase() {
+        assertEquals("4", FizzBuzz.fizzBuzz(4));
+        assertEquals("7", FizzBuzz.fizzBuzz(7));
+        assertEquals("8", FizzBuzz.fizzBuzz(8));
+    }
 }
